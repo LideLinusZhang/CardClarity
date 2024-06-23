e296fa554574ce2b16e3fedf679489bf836e5e6a
jeanniezhang <86752929+jeanniezhang@users.noreply.github.com>
2024-06-22 21:47:00 -0400

    home screen initial setup

--- app/src/main/java/edu/card/clarity/presentation/Home.kt
+++ app/src/main/java/edu/card/clarity/presentation/Home.kt
@@ -1,28 +1,137 @@
 package edu.card.clarity.presentation
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 @Composable
 fun HomeScreen() {
-    Box(
+    Column(
         modifier = Modifier
             .fillMaxSize()
-            .background(Color.Magenta),
-        contentAlignment = Alignment.Center
+            .background(Color.LightGray)
+            .padding(16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Top
+    ) {
+        Header(userName = "John Doe")
+
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .weight(1f)
+                .padding(vertical = 8.dp),
+            horizontalArrangement = Arrangement.SpaceEvenly,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            BarBox(label = "March")
+            BarBox(label = "April")
+            BarBox(label = "May")
+        }
+        Column( modifier = Modifier
+                .fillMaxWidth()
+                .weight(1f)
+            .padding(bottom = 8.dp),
+            verticalArrangement = Arrangement.SpaceEvenly,
+            horizontalAlignment = Alignment.CenterHorizontally)
+        {
+            CardBox(label = "My Cards")
+            CardBox(label = "Upcoming Payments")
+        }
+
+    }
+}
+
+@Composable
+fun Header(userName: String) {
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(bottom = 16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
     ) {
         Text(
-            text = "HOME",
+            text = "Hi $userName",
             fontWeight = FontWeight.Bold,
-            color = Color.White
+            fontSize = 24.sp,
+            color = Color.Black
+        )
+        Text(
+            text = "Your 3 month rewards summary",
+            fontWeight = FontWeight.Normal,
+            fontSize = 16.sp,
+            color = Color.Black
+        )
+    }
+}
+
+@Composable
+fun BarGroup(savingsOne: Number,
+             monthOne: String,
+             savingsTwo: Number,
+             monthTwo: String,
+             savingsThree: Number,
+             monthThree: String) {
+
+}
+
+@Composable
+fun Bar(label: String, savings: Number) {
+    Box(
+        modifier = Modifier
+            .fillMaxHeight()
+            .clip(RoundedCornerShape(8.dp))
+            .background(Color.Gray)
+            .padding(16.dp)
+    ) {
+        Text(
+            text = label,
+            fontWeight = FontWeight.Bold,
+            color = Color.Black
+        )
+    }
+}
+
+@Composable
+fun BarBox(label: String) {
+    Box(
+        modifier = Modifier
+            .fillMaxHeight()
+            .clip(RoundedCornerShape(8.dp))
+            .background(Color.Gray)
+            .padding(16.dp)
+    ) {
+        Text(
+            text = label,
+            fontWeight = FontWeight.Bold,
+            color = Color.Black
+        )
+    }
+}
+
+@Composable
+fun CardBox(label: String) {
+    Box(
+        modifier = Modifier
+            .fillMaxWidth()
+            .clip(RoundedCornerShape(8.dp))
+            .background(Color.Gray)
+            .padding(16.dp)
+    ) {
+        Text(
+            text = label,
+            fontWeight = FontWeight.Bold,
+            color = Color.Black
         )
     }
 }
