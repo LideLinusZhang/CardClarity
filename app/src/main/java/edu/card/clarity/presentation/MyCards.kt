9833ab415f7bae4b6ae59047c1956c1dd728dfb9
Sophie Chan <74480837+sophiec103@users.noreply.github.com>
2024-06-24 20:09:07 -0400

    My Cards Screen + Add Font (#4)
    Implemented My Cards screen UI
    (Currently unreachable screen by UI. Must create a link from Home screen
    after that is complete)
    Added "Patrick Hand" font (as per the Figma) as global font (this only
    shows when running the emulator, and the My Cards screen is not
    reachable on the emulator as of now)
    
    ![image](https://github.com/LideLinusZhang/cs446-project/assets/74480837/edd78557-02c3-4139-898c-7be10b9f264b)
    
    ![image](https://github.com/LideLinusZhang/cs446-project/assets/74480837/b5ee59fd-f189-401a-8fe4-95f848249fa4)

---
+++ app/src/main/java/edu/card/clarity/presentation/MyCards.kt
@@ -0,0 +1,154 @@
+package edu.card.clarity.presentation
+
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.CheckCircle
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import edu.card.clarity.ui.theme.CardClarityTheme
+import edu.card.clarity.ui.theme.Typography
+
+data class CardInfo(
+    val cardName: String,
+    val dueDate: String,
+    val backgroundColor: Color
+)
+
+@Composable
+fun MyCardsScreen() {
+    val cards = listOf(
+        CardInfo("TD Aeroplan Visa Infinite Card", "2024-05-24", Color(0xFFAED8FF)),
+        CardInfo("American Express Platinum Card", "2024-05-13", Color(0xFFB7FF9E)),
+        CardInfo("CIBC Dividend", "2024-06-03", Color(0xFFFF9EB8)),
+    )
+
+    CardClarityTheme {
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(Color.White)
+                .padding(20.dp)
+        ) {
+            Spacer(modifier = Modifier.height(16.dp))
+            Text(
+                text = "My Cards",
+                fontSize = 22.sp,
+                fontWeight = FontWeight.Bold,
+                fontFamily = Typography.bodyLarge.fontFamily,
+                modifier = Modifier.padding(bottom = 16.dp)
+            )
+            Spacer(modifier = Modifier.height(16.dp))
+            LazyColumn {
+                items(cards.size) { index ->
+                    CardItem(
+                        cardName = cards[index].cardName,
+                        dueDate = cards[index].dueDate,
+                        backgroundColor = cards[index].backgroundColor
+                    )
+                    Spacer(modifier = Modifier.height(24.dp))
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun CardItem(
+    cardName: String,
+    dueDate: String,
+    backgroundColor: Color
+) {
+    Card(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(190.dp),
+        shape = RoundedCornerShape(8.dp),
+        elevation = 4.dp
+    ) {
+        Row(
+            modifier = Modifier.fillMaxSize()
+        ) {
+            Box(
+                modifier = Modifier
+                    .weight(0.50f)
+                    .fillMaxHeight()
+                    .background(backgroundColor)
+                    .padding(16.dp)
+            ) {
+                Column(
+                    verticalArrangement = Arrangement.SpaceBetween,
+                    modifier = Modifier.fillMaxHeight()
+                ) {
+                    Text(
+                        text = cardName,
+                        fontWeight = FontWeight.Bold,
+                        fontSize = 18.sp
+                    )
+                    Spacer(modifier = Modifier.height(8.dp))
+                    Spacer(modifier = Modifier.weight(1f))
+                    OutlinedButton(
+                        onClick = { /* TODO: Handle receipts click */ },
+                        colors = ButtonDefaults.buttonColors(backgroundColor = Color.White),
+                        border = BorderStroke(1.5.dp, Color.Black),
+                        modifier = Modifier
+                            .align(Alignment.Start)
+                            .padding(bottom = 4.dp)
+                    ) {
+                        Text(text = "Receipts")
+                    }
+                }
+            }
+            Box(
+                modifier = Modifier
+                    .weight(0.45f)
+                    .fillMaxHeight()
+                    .background(color = Color(0xFFBFC0C2))
+                    .padding(end = 12.dp)
+            ) {
+                Column(
+                    horizontalAlignment = Alignment.End,
+                    verticalArrangement = Arrangement.Center,
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(8.dp)
+                ) {
+                    Spacer(modifier = Modifier.height(100.dp))
+                    Row {
+                        Icon(
+                            Icons.Default.CheckCircle,
+                            contentDescription = "Check Circle",
+                            modifier = Modifier
+                                .size(16.dp)
+                                .padding(end = 1.dp),
+                        )
+                        Text(
+                            text = "Payment Reminder",
+                            fontWeight = FontWeight.Bold,
+                        )
+                    }
+                    Spacer(modifier = Modifier.height(8.dp))
+                    Text(
+                        text = "Due Date: $dueDate",
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+@Preview
+fun MyCardsScreenPreview() {
+    MyCardsScreen()
+}
