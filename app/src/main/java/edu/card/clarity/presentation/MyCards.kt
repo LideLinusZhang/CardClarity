1a8aeb5af750c23c6862355b918379d523e19664
Linus Zhang <l625zhan@uwaterloo.ca>
2024-07-05 19:54:15 -0400

    Migrate from M2 to M3.

--- app/src/main/java/edu/card/clarity/presentation/MyCards.kt
+++ app/src/main/java/edu/card/clarity/presentation/MyCards.kt
@@ -2,13 +2,27 @@ package edu.card.clarity.presentation
 
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.RoundedCornerShape
-//noinspection UsingMaterialAndMaterial3Libraries
-import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.CheckCircle
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.OutlinedButton
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -75,7 +89,7 @@ fun CardItem(
             .fillMaxWidth()
             .height(190.dp),
         shape = RoundedCornerShape(8.dp),
-        elevation = 4.dp
+        elevation = CardDefaults.cardElevation(defaultElevation = 4.dp)
     ) {
         Row(
             modifier = Modifier.fillMaxSize()
@@ -100,7 +114,10 @@ fun CardItem(
                     Spacer(modifier = Modifier.weight(1f))
                     OutlinedButton(
                         onClick = { /* TODO: Handle receipts click */ },
-                        colors = ButtonDefaults.buttonColors(backgroundColor = Color.White),
+                        colors = ButtonDefaults.buttonColors(
+                            containerColor = Color.White,
+                            contentColor = Color.Black
+                        ),
                         border = BorderStroke(1.5.dp, Color.Black),
                         modifier = Modifier
                             .align(Alignment.Start)
