fae0a0512d5b43d99223f59bd1747c0a74e05408
Linus Zhang <l625zhan@uwaterloo.ca>
2024-07-09 17:15:17 -0400

    Implemented adapter and dependency injection for Places API to obtain the current place.

--- app/src/main/java/edu/card/clarity/dependencyInjection/PlacesModule.kt
+++ app/src/main/java/edu/card/clarity/dependencyInjection/PlacesModule.kt
@@ -1,21 +1,28 @@
 package edu.card.clarity.dependencyInjection
 
 import android.content.Context
-import com.google.android.gms.location.FusedLocationProviderClient
-import com.google.android.gms.location.LocationServices
+import com.google.android.libraries.places.api.Places
+import com.google.android.libraries.places.api.net.PlacesClient
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
+import edu.card.clarity.BuildConfig
 import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
-object LocationModule {
+object PlacesModule {
     @Singleton
     @Provides
-    fun provideFusedLocationProviderClient(
+    fun providePlacesClient(
         @ApplicationContext context: Context
-    ): FusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(context)
+    ): PlacesClient {
+        val apiKey = BuildConfig.PLACES_API_KEY
+
+        Places.initializeWithNewPlacesApiEnabled(context, apiKey)
+
+        return Places.createClient(context)
+    }
 }
