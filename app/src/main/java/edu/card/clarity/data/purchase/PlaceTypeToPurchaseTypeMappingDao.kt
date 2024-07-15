8ffe469a5a61823353919ffe293f6f06f5c45174
Linus Zhang <l625zhan@uwaterloo.ca>
2024-07-14 21:34:49 -0400

    Fixed compiler errors.

--- app/src/main/java/edu/card/clarity/data/purchase/PlaceTypeToPurchaseTypeMappingDao.kt
+++ app/src/main/java/edu/card/clarity/data/purchase/PlaceTypeToPurchaseTypeMappingDao.kt
@@ -9,11 +9,11 @@ import kotlinx.coroutines.flow.Flow
 @Dao
 interface PlaceTypeToPurchaseTypeMappingDao {
     @Query("SELECT purchaseType FROM placeTypeToPurchaseTypeMapping WHERE placeType = :placeType")
-    suspend fun observePurchaseTypeByPlaceType(placeType: String): Flow<PurchaseType>
+    fun observePurchaseTypeByPlaceType(placeType: String): Flow<PurchaseType>
 
     @Query("SELECT purchaseType FROM placeTypeToPurchaseTypeMapping WHERE placeType = :placeType")
-    fun getPurchaseTypeByPlaceType(placeType: String): PurchaseType?
+    suspend fun getPurchaseTypeByPlaceType(placeType: String): PurchaseType?
 
     @Upsert
-    fun upsert(mapping: PlaceTypeToPurchaseTypeMappingEntity)
+    suspend fun upsert(mapping: PlaceTypeToPurchaseTypeMappingEntity)
 }
