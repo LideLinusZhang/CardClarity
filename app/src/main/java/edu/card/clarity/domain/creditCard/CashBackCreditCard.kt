a3c8933b56312ca383d20d9caed778175f3dc7ff
Lide Linus Zhang <39998175+LideLinusZhang@users.noreply.github.com>
2024-07-22 23:53:49 -0400

    Add the Logic to Handle "Others" Purchase Type (#59)
    Say we want to purchase something at Canadian Tire, but none of our
    credit cards has a home improvement purchase reward. In this situation,
    the app should use "Others" purchase reward to determine the reward
    amount. This PR adds this logic and a safeguard to ensure there won't be
    a null reference exception.

--- app/src/main/java/edu/card/clarity/domain/creditCard/CashBackCreditCard.kt
+++ app/src/main/java/edu/card/clarity/domain/creditCard/CashBackCreditCard.kt
@@ -2,15 +2,20 @@ package edu.card.clarity.domain.creditCard
 
 import edu.card.clarity.domain.Purchase
 import edu.card.clarity.domain.PurchaseReward
+import edu.card.clarity.enums.PurchaseType
 
 data class CashBackCreditCard(
     override val info: CreditCardInfo,
     override val purchaseRewards: List<PurchaseReward>
 ) : ICreditCard {
     override fun getReturnAmountInCash(purchase: Purchase): Float {
-        val purchaseReward = purchaseRewards.first {
-            it.applicablePurchaseType == purchase.type
-        }
+        val purchaseReward = purchaseRewards.let {
+            it.firstOrNull { reward ->
+                reward.applicablePurchaseType == purchase.type
+            } ?: it.firstOrNull { reward ->
+                reward.applicablePurchaseType == PurchaseType.Others
+            }
+        } ?: return 0.0f
 
         return purchaseReward.getReturnAmount(purchase.total)
     }
