9b8417ef92e64624ab9348b901406fbb97e4bd3e
Linus Zhang <l625zhan@uwaterloo.ca>
2024-07-29 22:51:13 -0400

    Cleanup.

--- app/src/main/java/edu/card/clarity/dependencyInjection/DateFormatterModule.kt
+++ app/src/main/java/edu/card/clarity/dependencyInjection/DateFormatterModule.kt
@@ -5,7 +5,6 @@ import android.icu.text.SimpleDateFormat
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
-import dagger.hilt.android.components.ViewModelComponent
 import dagger.hilt.components.SingletonComponent
 import javax.inject.Singleton
 
